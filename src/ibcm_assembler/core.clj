4dd7983339c10d675b8fec022c5ad8771ccd9ae9
Koleman Nix <kolemannix@gmail.com>
2013-10-29 14:44:55 -0400

    add support for labels in ibcm code

--- src/ibcm_assembler/core.clj
+++ src/ibcm_assembler/core.clj
@@ -4,43 +4,53 @@
 (def hex-digits [\0 \1 \2 \3 \4 \5 \6 \7 \8 \9 \A \B \C \D \E \F])
 (def op-names [:halt :io :shift :load :store :add :sub :and :or :xor :not :nop :jmp :jmpe :jmpl :brl])
 (def opmap (zipmap op-names hex-digits))
-
-(def instruction-type #{:h :io :s :a :d})
+(def instruction-type #{:h :io :s :a :d :l})
+(def addresses (map #(format "%03x" %) (range 0 1000)))
 
 (defn parse-op 
   "takes a string representing an IBCM operation and returns the appropriate hex digit"
   [op]
   (opmap (keyword op)))
 
+(defn remove-first [input] (apply str (rest input)))
 (defn parse-instr [instr]
   "takes an instruction as a string and returns a map representing the instruction"
-  (let [instr-seq (re-seq #"\w+" instr)
+  (let [instr-seq (clojure.string/split instr #"\s+")
         opcode (first instr-seq)]
     (cond 
+      (or (empty? opcode) (= opcode "nop"))
+      {:instr-type :h :opcode :nop}
+      (.startsWith opcode ".")
+      {:instr-type :l}
       (= opcode "dw")
       {:instr-type :d :opcode :dw :data (nth instr-seq 1)}
-      (= opcode "nop")
-      {:instr-type :h :opcode :nop}
       (= opcode "halt")
       {:instr-type :h :opcode :halt}
       (= opcode "not")
       {:instr-type :h :opcode :not}
-      (.contains opcode "shift")
+      (.startsWith opcode "shift")
       {:instr-type :s :opcode :shift :rotate? false :shift-dir (if (= (nth opcode 5) \L) :left :right) :shift-count (nth instr-seq 1)}
-      (.contains opcode "rot")
+      (.startsWith opcode "rot")
       {:instr-type :s :opcode :shift :rotate? true :shift-dir (if (= (nth opcode 3) \L) :left :right) :shift-count (nth instr-seq 1)}
-      (.contains opcode "read")
+      (.startsWith opcode "read")
       {:instr-type :io :opcode :io :io-dir :read :io-format (if (= (nth opcode 4) \H) :hex :ascii)}
-      (.contains opcode "write")
+      (.startsWith opcode "print")
       {:instr-type :io :opcode :io :io-dir :write :io-format (if (= (nth opcode 5) \H) :hex :ascii)}
       :else {:instr-type :a :opcode (keyword opcode) :address (nth instr-seq 1)})))
 
-(defn encode-instruction [instr]
+(defn get-address [addr labelmap]
+  (if (contains? labelmap addr)
+    (labelmap addr)
+    addr))
+
+(defn encode-instruction [instr labels]
   "takes a map created by parse-instr and returns an encoded ibcm instruction"
   (def instr-type (instr :instr-type))
   (cond
     (= :d instr-type)
     (instr :data)
+    (= :l instr-type)
+    (str (opmap :nop) "000")
     (= :h instr-type) 
     (let [opcode (instr :opcode)
           opstr (opmap opcode)]
@@ -63,22 +73,38 @@
         (if rotate? "2C00" "2800")))
     (= :a instr-type)
     (let [address (instr :address)
-          opcode (instr :opcode)] (str (opmap opcode) address))))
+          opcode (instr :opcode)] (str (opmap opcode) (get-address address labels)))))
 
-(defn encode [instr] (.toUpperCase (encode-instruction (parse-instr instr))))
+(defn encode 
+  ([instr] 
+   (encode instr {}))
+  ([instr labelmap]
+   (encode-instruction (parse-instr instr) labelmap)))
 
 (defn pad [instr]
   (if (> (count instr) 5) instr (str instr \tab)))
+(defn format-instruction [op locn comm labelmap]
+  (str (encode op labelmap) \tab locn \tab (pad op) comm))
 
-(defn format-instruction [op locn comm]
-  (str (encode op) \tab locn \tab (pad op) comm))
+(defn make-pair [line locn] [line locn])
+;; TODO refactor label-map because i know there's about 1,000 better ways to do this
+(defn label-map [lines]
+  (let [with-dots  (->> (map make-pair lines addresses)
+                        (filter #(= (ffirst %) \.))
+                        flatten
+                        (apply hash-map))]
+    (zipmap (map remove-first (keys with-dots)) (vals with-dots))))
 
+;; TODO note, I think I could totally use the state monad here to handle labels
+;; I'm realizing that once I introduce a label map to these functions, I have two
+;; options: either add an argument, the label map, or pass in the state as a monad
 (defn assemble [x]
   (let [lines (clojure.string/split-lines (slurp x))
-        instrs (map first (map #(clojure.string/split % #";") lines))
         locns (map #(format "%03x" %) (range 0 1000))
-      comments (map (fn [x] (if (= 1 (count x)) "" (str \tab \; (second x)))) (map #(clojure.string/split % #";") lines))]
-    (apply str (interpose "\n" (map format-instruction instrs locns comments)))))
+        labels (repeat (count lines) (label-map lines)) 
+        instrs (map first (map #(clojure.string/split % #";") lines))
+        comments (map (fn [x] (if (= 1 (count x)) "" (str \tab \; (second x)))) (map #(clojure.string/split % #";") lines))]
+    (apply str (interpose "\n" (map format-instruction instrs locns comments labels)))))
 
 (defn -main [& args]
-  (spit "out.ibcm" (assemble (nth args 0))))
+  (spit (first args) (assemble (second args))))
--- test/ibcm_assembler/core_test.clj
+++ test/ibcm_assembler/core_test.clj
@@ -14,15 +14,19 @@
 (expect \4 (parse-op "store"))
 (expect \A (parse-op "not"))
 
-(expect "0000" (encode-instruction {:instr-type :h :opcode :halt}))
-(expect "2C00" (encode-instruction {:instr-type :s :opcode :shift :rotate? true :shift-dir :right}))
-(expect "1000" (encode-instruction {:instr-type :io :opcode :io :io-dir :read :io-format :hex}))
-(expect "1400" (encode-instruction {:instr-type :io :opcode :io :io-dir :read :io-format :ascii}))
-(expect "1800" (encode-instruction {:instr-type :io :opcode :io :io-dir :write :io-format :hex}))
-(expect "1C00" (encode-instruction {:instr-type :io :opcode :io :io-dir :write :io-format :ascii}))
-(expect "5100" (encode-instruction {:instr-type :a :opcode :add :address 100}))
-(expect "6450" (encode-instruction {:instr-type :a :opcode :sub :address 450}))
+(expect "B000" (encode ".start"))
+(expect "C000" (encode "jmp 000"))
+(expect {"a" "001", "b" "002", "c" "003", "start" "009"} (label-map (clojure.string/split-lines (slurp "resources/labels.sibcm"))))
+(expect {} (label-map (clojure.string/split-lines (slurp "resources/test2.sibcm"))))
 
+(expect "0000" (encode-instruction {:instr-type :h :opcode :halt} {}))
+(expect "2C00" (encode-instruction {:instr-type :s :opcode :shift :rotate? true :shift-dir :right} {}))
+(expect "1000" (encode-instruction {:instr-type :io :opcode :io :io-dir :read :io-format :hex} {}))
+(expect "1400" (encode-instruction {:instr-type :io :opcode :io :io-dir :read :io-format :ascii} {}))
+(expect "1800" (encode-instruction {:instr-type :io :opcode :io :io-dir :write :io-format :hex} {}))
+(expect "1C00" (encode-instruction {:instr-type :io :opcode :io :io-dir :write :io-format :ascii} {}))
+(expect "5100" (encode-instruction {:instr-type :a :opcode :add :address 100} {}))
+(expect "6450" (encode-instruction {:instr-type :a :opcode :sub :address 450} {}))
 (expect {:instr-type :s :opcode :shift :rotate? true :shift-dir :right :shift-count "4"} (parse-instr "rotR 4"))
 (expect {:instr-type :s :opcode :shift :rotate? false :shift-dir :left :shift-count "100"} (parse-instr "shiftL 100"))
 
@@ -32,17 +36,17 @@
 (expect "5100" (encode "add 100"))
 (expect "1400" (encode "readC"))
 (expect "1000" (encode "readH"))
-(expect "1800" (encode "writeH"))
-(expect "1C00" (encode "writeC"))
+(expect "1800" (encode "printH"))
+(expect "1C00" (encode "printC"))
 
 (expect "CA" (encode "jmp A"))
 (expect "0003" (encode "dw 0003"))
 (expect "B000" (encode "nop"))
 
+(expect "001" (get-address "001" {"a" "001" "start" "0aa"}))
+(expect "0aa" (get-address "start" {"a" "001" "start" "0aa"}))
+
 (expect "halt\t" (pad "halt"))
 (expect "add 100" (pad "add 100"))
-
-(expect (slurp "resources/test1.ibcm") (assemble "resources/test1.sibcm"))
-;(expect "C00A\t000\n0003\t001\nB000\t002\nB000\t003\nB000\t004\nB000\t005\nB000\t006\nB000\t007\nB000\t008\nB000\t009\n3001\t00a\n1800\t00b" (assemble "resources/test2.sibcm"))
 
 (expect "5100\t000\tadd 100\n0000\t001\thalt\t" (assemble "resources/test0.sibcm"))
